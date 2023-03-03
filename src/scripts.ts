9f9a2004729840d40b16153c39beda7bf1a957a6
IVIosab <mosab.f.r@gmail.com>
2023-03-04 00:00:26 +0300

    [fix][homework3] adding some missing typos and reorganizing folder structure

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,44 +1,61 @@
 interface Comic {
-    alt: string,
-    day: string,
-    img: string,
-    link: string,
-    month: string,
-    news: string,
-    num: number, 
-    safe_title: string, 
-    title: string,
-    transcript: string, 
-    year: string
+  alt: string;
+  day: string;
+  img: string;
+  link: string;
+  /** Month starting from 1 for January */
+  month: string;
+  news: string;
+  num: number;
+  safe_title: string;
+  title: string;
+  transcript: string;
+  year: string;
 }
 
-function getId(){
-    const params = new URLSearchParams()
-    params.append("email", "o.mohamed@innopolis.universtiy")
-    return fetch('https://fwd.innopolis.app/api/hw2?'+params.toString())
-    .then(r => r.json());
+async function getId() {
+  const params = new URLSearchParams();
+  params.append("email", "o.mohamed@innopolis.universtiy");
+  const res = await fetch(
+    "https://fwd.innopolis.app/api/hw2?" + params.toString()
+  );
+  const id: number = await res.json();
+  return id;
 }
 
-function getComic(id: number){
-    const params = new URLSearchParams()
-    params.append("num", id.toString())
-    return fetch('https://getxkcd.vercel.app/api/comic?'+params.toString())
-    .then(r => r.json());
+async function getComic(id: number) {
+  const params = new URLSearchParams();
+  params.append("num", id.toString());
+  const res = await fetch(
+    "https://getxkcd.vercel.app/api/comic?" + params.toString()
+  );
+  const object: Comic = await res.json();
+  return await res.json();
 }
 
-window.addEventListener('load', async function(e){
-    const id = await getId();
-    const comic = await getComic(id)
-    displayComic(comic)
-})
+window.addEventListener("load", async function (e) {
+  const id = await getId();
+  const comic = await getComic(id);
+  console.log(comic);
+  displayComic(comic);
+});
 
-async function displayComic(comicObj: Comic){
-    const comicImg = document.getElementById("comic-img") as HTMLImageElement
-    const comicTitle = document.getElementById("comic-title") as HTMLHeadingElement
-    const comicDate = document.getElementById("comic-date") as HTMLHeadingElement
-    comicTitle.textContent = comicObj.title 
-    comicImg.alt = comicObj.alt
-    comicImg.src = comicObj.img
-    const date = new Date(comicObj.year.toString()+"-"+comicObj.month.toString()+"-"+comicObj.day.toString())
-    comicDate.textContent = date.toLocaleDateString('en-us', {weekday: 'long', year: 'numeric',  month: 'long', day: 'numeric'})
+
+async function displayComic(comicObj: Comic) {
+  const comicImg = document.getElementById("comic-img") as HTMLImageElement;
+  const comicTitle = document.getElementById(
+    "comic-title"
+  ) as HTMLHeadingElement;
+  const comicDate = document.getElementById("comic-date") as HTMLHeadingElement;
+  const { year, month, day } = comicObj;
+  comicTitle.textContent = comicObj.title;
+  comicImg.alt = comicObj.alt;
+  comicImg.src = comicObj.img;
+  const date = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+  comicDate.textContent = date.toLocaleDateString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 }
