133e29b7de448697884afdba7d656909b0c4fe6f
IVIosab <mosab.f.r@gmail.com>
2023-03-04 17:26:20 +0300

    [fix][homework3] minor bug fixes and adding some comments and types

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,22 +1,27 @@
 interface Comic {
   alt: string;
+  /** string value representing the day */
   day: string;
+  /** represents the comic image link */
   img: string;
   link: string;
-  /** Month starting from 1 for January */
+  /** string value representing the month starting from 1 for January */
   month: string;
+  /** */
   news: string;
+  /** number that represents the comic id */
   num: number;
   safe_title: string;
   title: string;
   transcript: string;
+  /** string value representing the year */
   year: string;
 }
 
-async function getId() {
+async function getId(){
   const params = new URLSearchParams();
-  params.append("email", "o.mohamed@innopolis.universtiy");
-  const res = await fetch(
+  params.append("email", "o.mohamed@innopolis.university");
+  const res: Response = await fetch(
     "https://fwd.innopolis.app/api/hw2?" + params.toString()
   );
   const id: number = await res.json();
@@ -26,32 +31,39 @@ async function getId() {
 async function getComic(id: number) {
   const params = new URLSearchParams();
   params.append("num", id.toString());
-  const res = await fetch(
+  const res: Response = await fetch(
     "https://getxkcd.vercel.app/api/comic?" + params.toString()
   );
   const object: Comic = await res.json();
-  return await res.json();
+  return object;
 }
 
 window.addEventListener("load", async function (e) {
-  const id = await getId();
-  const comic = await getComic(id);
+  const id: number = await getId();
+  const comic: Comic = await getComic(id);
   console.log(comic);
   displayComic(comic);
 });
 
-
 async function displayComic(comicObj: Comic) {
-  const comicImg = document.getElementById("comic-img") as HTMLImageElement;
+  const comicImg = document.getElementById(
+    "comic-img"
+  ) as HTMLImageElement;
   const comicTitle = document.getElementById(
     "comic-title"
   ) as HTMLHeadingElement;
-  const comicDate = document.getElementById("comic-date") as HTMLHeadingElement;
+  const comicDate = document.getElementById(
+    "comic-date"
+  ) as HTMLHeadingElement;
+  
   const { year, month, day } = comicObj;
+  
   comicTitle.textContent = comicObj.title;
   comicImg.alt = comicObj.alt;
   comicImg.src = comicObj.img;
+  
   const date = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+  
   comicDate.textContent = date.toLocaleDateString("en-us", {
     weekday: "long",
     year: "numeric",
